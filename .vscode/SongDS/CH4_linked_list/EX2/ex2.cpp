4e9708f095722993a491ea30886fd95b3464f12b
minseok0404 <minsuck040484@gmail.com>
2021-10-29 23:29:40 +0900

    .

---
+++ .vscode/SongDS/CH4_linked_list/EX2/ex2.cpp
@@ -0,0 +1,21 @@
+#include "linked_list.h"
+
+
+int main(void)
+{
+    //일단 chain하나 만들기
+    Chain chain;
+    ChainNode *CN = chain.getFirst();
+    for (int i = 0; i < 10; i++)
+        CN=chain.Insert(CN, i + 1);
+    chain.Show();
+    cout<<endl;
+    
+    //chain의 4번째 노드 삭제해보기
+    ChainNode* temp = chain.getFirst();
+    for(int i = 0; i <3; i++)
+        temp = temp -> getLink(); //
+    chain.Delete(temp);
+    chain.Show();
+}
+ 
